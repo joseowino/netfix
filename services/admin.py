15b13b45083a082536ca4f5adfce6400daf88abc
joseowin <joeoowino@gmail.com>
2025-07-03 11:56:32 +0300

    feat: Regiter service model to admin.py

--- services/admin.py
+++ services/admin.py
@@ -1,8 +1,6 @@
+# services/admin.py
 from django.contrib import admin
+from .models import Service, ServiceRequest
 
-from .models import Service
-
-
-@admin.register(Service)
-class ServiceAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "price_hour", "field", "date")
+admin.site.register(Service)
+admin.site.register(ServiceRequest)

6a115268fa9472ab95796662ca7c14fe734e4273
joseowin <joeoowino@gmail.com>
2025-07-09 13:53:44 +0300

    refactor: Clean up URL patterns by removing unused imports and comments

--- main/urls.py
+++ main/urls.py
@@ -1,21 +1,9 @@
-from django.urls import path, include
-from django.conf.urls.static import static
+from django.urls import path
 from . import views as v
-from django.contrib import admin
-from django.conf import settings
 
 app_name = "main"
 
 urlpatterns = [
     path('', v.home, name='home'),
-    path('logout/', v.logout, name='logout'),
-
-    path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
-    path('services/', include('services.urls')),
+    path('logout/', v.logout, name='logout')
 ]
-
-# Serve media files in development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
